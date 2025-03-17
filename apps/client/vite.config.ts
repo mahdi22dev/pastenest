562aee14a0103b06dca7508340f76828eb201807
mahdi22dev <idrissimahdi2020@gmail.com>
2025-03-17 04:51:07 +0000

    fix

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,18 +1,42 @@
-import { defineConfig } from "vite";
+// import { defineConfig, loadEnv } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": { target: "http://localhost:3000" },
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
+
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": { target: "http://localhost:3000" },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_SERVER_PATH,
+          changeOrigin: true,
+        },
+      },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
