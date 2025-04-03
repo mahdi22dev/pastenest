867e35a4193254afe37b37aa7a9ecfbe56ad2a01
mahdi22dev <idrissimahdi2020@gmail.com>
2025-04-03 00:04:09 +0000

    pew

--- apps/client/src/components/paste-form.tsx
+++ apps/client/src/components/paste-form.tsx
@@ -19,7 +19,6 @@ import { PasteBody } from "@/lib/types";
 import { PulseLoader } from "react-spinners";
 import ReCAPTCHA from "react-google-recaptcha";
 import { useNavigate } from "react-router-dom";
-import { Paste } from "pastenest-types";
 
 export function PasteForm() {
   const [syntax, setSyntax] = useState("syntax");
@@ -46,19 +45,18 @@ export function PasteForm() {
         },
         body: JSON.stringify(body),
       });
-      const data = (await response.json()) as Paste;
 
-      // const data = (await response.json()) as {
-      //   id: number;
-      //   pasteId: string;
-      //   title: string;
-      //   content: string;
-      //   createdAt: string;
-      //   experation: any;
-      //   mode: string;
-      //   password: any;
-      //   authorId: number;
-      // };
+      const data = (await response.json()) as {
+        id: number;
+        pasteId: string;
+        title: string;
+        content: string;
+        createdAt: string;
+        experation: any;
+        mode: string;
+        password: any;
+        authorId: number;
+      };
 
       if (data?.pasteId) {
         return navigate("/" + data.pasteId);
